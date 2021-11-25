5c45da424cedbc7d9c0fe68a01dfa809b8d0724a
seumbae <ohcm0013@gmail.com>
2021-11-26 03:34:32 +0900

    new branch

--- mcu1.0.1/mcu1.0.1.ino
+++ mcu1.0.1/mcu1.0.1.ino
@@ -10,13 +10,16 @@ const char* host = "121.191.167.72";
 WiFiServer server(80);
 WiFiClient client;
 SoftwareSerial mcu(D6, D5);
-String response;
+
+const char* flag;
 float temp;
+int motorCycle = 30; //Default 30min
+String response;
 
 void setup() {
   Serial.begin(115200);
   mcu.begin(9600);
-   
+  
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
@@ -37,38 +40,66 @@ void setup() {
 
   client = server.available();
 
-  //getData();
-  //createAE();
-  WriteTempData();
+  //getDataFromMobius();
+  //servoFlag();
+  //WriteTempData();
+  
 }
  
 void loop() {
-  //getTempFromUno();
+  getDataFromUno();
+  getDataFromMobius(); 
 }
 
-float getTempFromUno(){
-  StaticJsonBuffer<1000> jsonBuffer;
-  JsonObject& data = jsonBuffer.parseObject(mcu);
+void getDataFromUno(){
+    StaticJsonBuffer<1000> jsonBuffer;
+    JsonObject& data = jsonBuffer.parseObject(mcu);
 
-  Serial.print("Recieved temp: ");
-  float temp = data["temp"];
-  Serial.println(temp);
-  delay(2000);
-  return temp;
+    Serial.print("Recieved temp: ");
+    temp = data["temp"];
+    Serial.println(temp);
+    delay(2000);
 }
-
-void servo(){
-    if(temp > 20){
-      http.begin(client,"http://121.191.167.72:7579/Mobius/test/servo");
-
-      http.addHeader("Accept","application/json");
-      http.addHeader("X-M2M-RI", "12345");
-      http.addHeader("X-M2M-Origin", "S");
-      http.addHeader("Content-Type", "application/json;ty=4");
-
-      String body = "{\"m2m:cin\": {\"con\": \"" + (String)temp + "\"}}";
-    }
+void runServo(){
+  Serial.println("Motor Running");
+  
 }
+//void servoFlag(){
+//    HTTPClient http;
+//    String body;
+//    
+//    if(temp > 20){
+//      http.begin(client,"http://121.191.167.72:7579/Mobius/test/servo");
+//
+//      http.addHeader("Accept","application/json");
+//      http.addHeader("X-M2M-RI", "12345");
+//      http.addHeader("X-M2M-Origin", "S");
+//      http.addHeader("Content-Type", "application/json;ty=4");
+//
+//      body = "{\"m2m:cin\": {\"con\": \"on\"}}";
+//    }
+//    else{
+//      http.begin(client,"http://121.191.167.72:7579/Mobius/test/servo");
+//
+//      http.addHeader("Accept","application/json");
+//      http.addHeader("X-M2M-RI", "12345");
+//      http.addHeader("X-M2M-Origin", "S");
+//      http.addHeader("Content-Type", "application/json;ty=4");
+//
+//      body = "{\"m2m:cin\": {\"con\": \"off\"}}";
+//    }
+//    int httpCode = http.POST(body);
+//    const char* ans = getData("servo");
+//    uno.begin(9600);
+//    const char* ans = "on";
+//    StaticJsonBuffer<1000> jsonBuffer;
+//    JsonObject& data = jsonBuffer.createObject();
+// 
+//    data["flag"] = ans;
+//    data.printTo(uno);
+//    jsonBuffer.clear();
+//    
+//}
 
 void WriteTempData(){
   HTTPClient http;
@@ -79,7 +110,7 @@ void WriteTempData(){
   http.addHeader("X-M2M-Origin", "S");
   http.addHeader("Content-Type", "application/json;ty=4");
 
-  temp = getTempFromUno();
+  //temp = getTempFromUno();
   String body = "{\"m2m:cin\": {\"con\": \"" + (String)temp + "\"}}";
   
   int httpCode = http.POST(body);
@@ -88,9 +119,10 @@ void WriteTempData(){
   http.end();
 }
 
-void getData(){
+void getDataFromMobius(){
   HTTPClient http;
-  http.begin(client,"http://121.191.167.72:7579/Mobius/test/tempData/latest"); // path
+
+  http.begin(client,"http://121.191.167.72:7579/Mobius/test/servo/latest"); // path
   
   //Definition Header
   http.addHeader("Accept","application/json");
@@ -98,8 +130,7 @@ void getData(){
   http.addHeader("X-M2M-Origin", "S");
 
   int httpCode = http.GET();
-  
-  Serial.println(httpCode);
+ 
   if(httpCode > 0){
     response = http.getString();
     
@@ -109,26 +140,34 @@ void getData(){
     JsonObject& root = jsonBuffer.parseObject(response);
     JsonObject& m2m_cin = root["m2m:cin"];
     
-    const char* data = m2m_cin["con"]; // "27"
-    Serial.println(data);
+    flag = m2m_cin["con"]; // "27"
+    Serial.print("Flag is "); Serial.println(flag);
   }
-  http.end();
 
-}
-
-void createAE(){
-  HTTPClient http;
-  http.begin(client,"http://121.191.167.72:7579/Mobius"); // path
+  http.begin(client,"http://121.191.167.72:7579/Mobius/test/defaultMotorCycle/latest"); // path
   
   //Definition Header
   http.addHeader("Accept","application/json");
   http.addHeader("X-M2M-RI", "12345");
   http.addHeader("X-M2M-Origin", "S");
-  http.addHeader("Content-Type", "application/json;ty=2");
 
-  String body = "{\"m2m:ae\":{\"rn\": \"test\",\"api\": \"0.2.481.2.0001.001.000111\",\"lbl\": [\"key1\", \"key2\"],\"rr\": true,\"poa\": [\"http://203.254.173.104:9727\"]}}";
-  int httpCode = http.POST(body); 
+  httpCode = http.GET();
+ 
+  if(httpCode > 0){
+    response = http.getString();
+    
+    const size_t capacity = JSON_OBJECT_SIZE(1) + JSON_OBJECT_SIZE(11) + 180;
+    DynamicJsonBuffer jsonBuffer(capacity);
 
-  Serial.println(httpCode);
+    JsonObject& root = jsonBuffer.parseObject(response);
+    JsonObject& m2m_cin = root["m2m:cin"];
+    Serial.print("Default vaule: "); Serial.println(motorCycle);
+    if(motorCycle != m2m_cin["con"]){
+      motorCycle = m2m_cin["con"];
+      runServo();
+    }
+    
+  }
   http.end();
+
 }
--- uno1.0.1/uno1.0.1.ino
+++ uno1.0.1/uno1.0.1.ino
@@ -4,21 +4,60 @@
 #include <DS18B20.h>
 #include <OneWire.h>
 
-SoftwareSerial mcu(5,6);
+#include <Servo.h>
+
+SoftwareSerial mcu(5,6); //Rx, TX
 DS18B20 DS18B20_Sensor(7);
+float temp;
 
 void setup() {
   Serial.begin(9600);
 
   mcu.begin(9600);
+  //servo.attach(8);
 }
 
 void loop() {
-  StaticJsonBuffer<1000> jsonBuffer;
+  tempToMcu();
+  //servoFlag();
+  //servoSweep(flag);
+
+}
+
+void tempToMcu(){
+  StaticJsonBuffer<500> jsonBuffer;
   JsonObject& data = jsonBuffer.createObject();
  
-  data["temp"] = DS18B20_Sensor.getTempC();
+  temp = DS18B20_Sensor.getTempC();
+  data["temp"] = temp;
   data.printTo(mcu);
+  Serial.println(temp);
   jsonBuffer.clear();
   delay(2000);
 }
+//void servoSweep(String flag){
+//  int pos;
+//  //servoFlag();
+//  if(flag =="on"){
+//    //open
+//    for(pos = 0 ;pos <= 180; pos +=1){
+//      servo.write(pos);
+//    } 
+//    delay(5000);
+//    //close
+//    for (pos = 180; pos >= 0; pos -=1) {
+//      servo.write(pos);              
+//    }
+//  }
+//}
+//void servoFlag(){
+//    uno.begin(9600);
+//    StaticJsonBuffer<1000> jsonBuffer;
+//    JsonObject& data = jsonBuffer.parseObject(uno);
+//
+//    Serial.print("Recieved flag: ");
+//    flag = data["flag"];
+//    Serial.println(flag);  
+//    delay(3000);
+//  
+//}
